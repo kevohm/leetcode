8efdbee9615f0034453646840e39c51f54bce19d
kevohm <kevinkbet@gmail.com>
2023-06-03 22:26:00 +0300

    added new

--- Longest_substring/Lonsgest_substring.py
+++ Longest_substring/Lonsgest_substring.py
@@ -1,30 +1,47 @@
-
 class Solution(object):
+    ans = []
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        ans = []
+        self.recursive_search(s)
+        len_arr = self.check_length(Solution.ans)
+        Solution.ans = []
+        return len_arr
+
+    def recursive_search(self, s, i=0):
+        if i >= len(s):
+            return
         arr = []
+        for k in range(0,len(s)):
+            arr = []
+            arr =  s[i:len(s) - k]
+            if(len(arr) > 0):
+                self.add_data(arr)
+        self.recursive_search(s, i+1)
+    
+    def check_rep(self, s):
+        arr = []
+        if s is None:
+            return None
         for i in s:
             if i in arr:
-                ans.append(arr)
-                arr = []
-                arr.append(i)
-            else:
-                arr.append(i)
-        ans.append(arr)
-        if len(ans) > 0:
-            max = len(ans[0])
-            data = ans[0]
-            for i in range(len(ans)):
-                l = len(ans[i])
-                if l > max:
-                    max = l
-                    data = ans[i]
-            print(data)
-            return len(data)
-        return len(arr)
-
-Solution().lengthOfLongestSubstring("aab")
+                return None
+            arr.append(i)
+        return arr
+    
+    def add_data(self, arr):
+        array = self.check_rep(arr)
+        if array is not None:
+            if array not in Solution.ans:
+                Solution.ans.append(array)
+    
+    def check_length(self, arr):
+        if len(arr) < 1:
+            return 0
+        max = arr[0]
+        for i in arr:
+            if len(i) > len(max):
+                max = i
+        return len(max)
